97c457b4602b6c511d890f4e6521c9753f80b3f4
Rowan Ferrabee <rowanferrabee@Rowans-MBP.bluewireless.asia>
2018-03-16 03:24:08 +0800

    Add real time graph updates

--- public/js/sb-admin-charts.js
+++ public/js/sb-admin-charts.js
@@ -2,57 +2,82 @@
 // -- Set new default font family and font color to mimic Bootstrap's default styling
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
-// -- Bar Chart Example
-var ctx = document.getElementById("myBarChart");
-var myLineChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ["Kitchen Lights", "Oven", "Air Conditioning", "Clothes Dryer"],
-    datasets: [{
-      label: "Revenue",
-      backgroundColor: "rgba(2,117,216,1)",
-      borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841],
-    }],
-  },
-  options: {
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'month'
+
+function loadMonitorData() {
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var jsonObj = JSON.parse(this.responseText);
+
+      var counter = 0;
+      var powerData = [0,0,0,0];
+      for (var i in jsonObj) {
+        powerData[counter] = jsonObj[i];
+        counter += 1;
+      }
+
+      var ctx = document.getElementById("myBarChart");
+      var myLineChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: ["Kitchen Lights", "Oven", "Air Conditioning", "Clothes Dryer"],
+          datasets: [{
+            label: "W",
+            backgroundColor: ['#007bff', '#dc3545', '#28a745', '#ffc107'],
+            borderColor: "rgba(2,117,216,1)",
+            data: powerData,
+          }],
         },
-        gridLines: {
-          display: false
-        },
-        ticks: {
-          maxTicksLimit: 6
+        options: {
+          scales: {
+            xAxes: [{
+              time: {
+                unit: 'W'
+              },
+              gridLines: {
+                display: false
+              },
+              ticks: {
+                maxTicksLimit: 6
+              }
+            }],
+            yAxes: [{
+              ticks: {
+                min: 0,
+                max: 1000,
+                maxTicksLimit: 5
+              },
+              gridLines: {
+                display: true
+              }
+            }],
+          },
+          legend: {
+            display: false
+          },
+          animation: false
         }
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          max: 15000,
-          maxTicksLimit: 5
+      });
+      // -- Pie Chart Example
+      var ctx = document.getElementById("myPieChart");
+      var myPieChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+          labels: ["Kitchen Lights", "Oven", "Air Conditioning", "Clothes Dryer"],
+          datasets: [{
+            data: powerData,
+            backgroundColor: ['#007bff', '#dc3545', '#28a745', '#ffc107'],
+          }],
         },
-        gridLines: {
-          display: true
+        options: {
+          animation: false
         }
-      }],
-    },
-    legend: {
-      display: false
+      });
     }
-  }
-});
-// -- Pie Chart Example
-var ctx = document.getElementById("myPieChart");
-var myPieChart = new Chart(ctx, {
-  type: 'pie',
-  data: {
-    labels: ["Kitchen Lights", "Clothes Dryer", "Oven", "Air Conditioning"],
-    datasets: [{
-      data: [9.81, 15.58, 17.40, 57.21],
-      backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745'],
-    }],
-  },
-});
+  };
+  xhttp.open("GET", "read", true);
+  xhttp.send();
+
+  setTimeout(loadMonitorData, 300);
+}
